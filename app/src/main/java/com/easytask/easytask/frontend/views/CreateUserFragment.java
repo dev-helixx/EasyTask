15447725e78704935d222037a76d97d5a1e95112
Silas El-Azm Stryhn <silas_es29@hotmail.com>
2017-10-11 14:17:17 +0200

    Firebase Auth working for sign in!

--- app/src/main/java/com/easytask/easytask/frontend/views/CreateUserFragment.java
+++ app/src/main/java/com/easytask/easytask/frontend/views/CreateUserFragment.java
@@ -108,13 +108,13 @@ public class CreateUserFragment extends Fragment implements View.OnClickListener
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
+//                            Log.d(TAG, "createUserWithEmail:success");
 //                            FirebaseUser user = firebaseAuth.getCurrentUser();
 //                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(getContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+//                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                            Toast.makeText(getContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
 //                            updateUI(null);
                         }
 
--- app/src/main/java/com/easytask/easytask/frontend/views/LoginActivity.java
+++ app/src/main/java/com/easytask/easytask/frontend/views/LoginActivity.java
@@ -3,10 +3,12 @@ package com.easytask.easytask.frontend.views;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -15,6 +17,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.easytask.easytask.R;
 import com.easytask.easytask.util.Validator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -42,6 +47,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         setContentView(R.layout.activity_login);
 
+        firebaseAuth = FirebaseAuth.getInstance();
 
         /* Instantiate views*/
         mainLoginBtn = (Button)findViewById(R.id.main_login_button);
@@ -62,7 +68,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+//        Toast.makeText(this, currentUser.toString(), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onClick(View v) {
@@ -83,10 +95,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 /* Validate both username and password */
                 if(validator.checkEmail(email) && validator.checkPassword(password))
                 {
-                    Toast.makeText(this, "LOGIN SUCCESS", Toast.LENGTH_SHORT).show();
 
-                    Intent mainscreen = new Intent(this, MainActivity.class);
-                    startActivity(mainscreen);
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(LoginActivity.this, "LOGIN SUCCESS", Toast.LENGTH_SHORT).show();
+
+                                        Intent mainscreen = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(mainscreen);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
                 }
                 else
                 {
