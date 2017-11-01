5c4eeb073504295b16f048ff8a05349870f0a8f4
LAPTOP-0S945DAN\Ramyar Hassani <ramyar6@hotmail.com>
2017-11-01 14:27:39 +0100

    test

---
+++ app/src/main/java/com/easytask/easytask/frontend/views/TaskDescriptionActivity.java
@@ -0,0 +1,15 @@
+package com.easytask.easytask.frontend.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.easytask.easytask.R;
+
+public class TaskDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_description);
+    }
+}
