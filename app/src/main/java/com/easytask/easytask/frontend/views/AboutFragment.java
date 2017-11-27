0abb36df2ba859a8fa0624a61c3216a7f0720abc
Silas El-Azm Stryhn <silas_es29@hotmail.com>
2017-11-27 21:20:29 +0100

    lol

--- app/src/main/java/com/easytask/easytask/frontend/views/AboutFragment.java
+++ app/src/main/java/com/easytask/easytask/frontend/views/AboutFragment.java
@@ -33,21 +33,8 @@ public class AboutFragment extends Fragment {
 
         about_label = (TextView) view.findViewById(R.id.about_label);
 
-        about_label.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-                "Nam tincidunt fringilla erat. Integer euismod malesuada metus, non efficitur libero egestas ac. " +
-                "Curabitur sed tempus urna. Vivamus tempus consectetur turpis, vitae blandit mi dapibus nec. " +
-                "Quisque tempor, ex at ornare maximus, metus nisl facilisis nisl, ac gravida ante felis quis lectus. " +
-                "Sed ullamcorper lectus mi, nec auctor lorem pretium ac. Donec lobortis, nulla at porta molestie, " +
-                "sem turpis ullamcorper ante, vitae condimentum nibh ante sit amet justo. Integer sapien lacus, " +
-                "ultrices eu mi quis, dignissim sollicitudin enim. Proin ante lectus, vehicula ac efficitur at, " +
-                "porta fringilla eros. Mauris sodales sed metus sodales viverra. Integer viverra pretium ipsum, id " +
-                "efficitur orci eleifend id. Aliquam id gravida ipsum. Nullam congue magna non tristique convallis. " +
-                "Pellentesque dapibus accumsan orci ac porttitor. In hac habitasse platea dictumst. Morbi commodo " +
-                "elementum lacus ut imperdiet. Donec pellentesque sem tellus, accumsan feugiat purus mattis sed. " +
-                "Vestibulum commodo placerat elementum. In tincidunt porta suscipit. Donec vitae fermentum erat, " +
-                "et scelerisque erat. Vestibulum ultricies gravida nulla, a aliquet massa commodo ac. Aliquam ac " +
-                "consequat nulla. Sed vel massa a nibh hendrerit feugiat. Nullam tempor, mi nec interdum tempor, " +
-                "diam libero volutpat sapien, et venenatis mi diam non nisl.");
+        about_label.setText("Hej og velkommen til EasyTask - App'en der gør din hverdag nemmere!" +
+                "Med denne app kan du nemt og hurtigt oprette opgaver som du ikke selv kan overskue at skulle lave.");
     }
 
 
